a4df56de793c5912fb119dca36cfbe0913f0a59c
FirePlank <fireplankbusiness@gmail.com>
2021-03-07 21:22:40 +0200

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-from pyclick import HumanClicker
-import pytweening
-import pyautogui
-from pyclick.humancurve import HumanCurve
-hc = HumanClicker()
-curve = HumanCurve(pyautogui.position(),(1000,1000), distortionFrequency=0, tweening=pytweening.easeInOutQuad, offsetBoundaryY=0, offsetBoundaryX=0)
-hc.move((100,100), duration=0.1, humanCurve=curve)
